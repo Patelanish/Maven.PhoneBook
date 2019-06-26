c32b35fafa619b6b49c841490537620f4cd42498
Anish Patel <patelanish223609@gmail.com>
2019-06-26 15:53:39 -0400

    Don't have all the test passing because I am told to use linkedhash map but I am not 100% how to use it so I will get a tutor and finish it up

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,76 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    List<String> listPhoneNames = new ArrayList<>();
+    List<String> ListNames = new ArrayList<>();
+    Map<String, List<String>> map = new LinkedHashMap<>();
+
+
+
+
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.map = map;
+
+
+
     }
 
     public PhoneBook() {
-        this(null);
+        this(new LinkedHashMap<String, List<String>>());
     }
 
     public void add(String name, String phoneNumber) {
+        if(map.containsKey(name))
+            map.get(name).add(phoneNumber);
+        else {
+            map.put(name, listPhoneNames);
+            add(name, phoneNumber);
+        }
+
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        map.put(name, Arrays.asList(phoneNumbers));
     }
 
     public void remove(String name) {
+        map.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
-    }
+        String phoneNumber = null;
+        if(listPhoneNames.contains(phoneNumber)){
+            return true;}
+            return false;
+        }
+
 
     public List<String> lookup(String name) {
-        return null;
+        return map.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
-        return null;
+        String result = "";
+        for (Map.Entry<String, List<String>> entry: map.entrySet() )
+            result = entry.getKey();
+        return result;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        listPhoneNames.addAll(map.keySet());
+        return listPhoneNames;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return this.map;
+    }
+
+    private class Entry<T, T1> {
     }
 }
